1e773431e98e97ec9c1158909e4b2f265d4f549e
Imed Jadli <imedjadli@Imeds-MacBook-Air.local>
2026-09-08 10:39:12 +0100

    add app component test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
